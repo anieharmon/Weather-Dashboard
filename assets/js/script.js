206039d69749b35588bc88168e0022448618b47d
anieharmon <anieharmon@icloud.com>
2023-08-07 01:05:15 -0600

    hsitory

--- assets/js/script.js
+++ assets/js/script.js
@@ -101,48 +101,49 @@ function initPage() {
        });
 }
 
+ // Get history from local storage if any
+ searchEl.addEventListener("click", function () {
+  const searchTerm = cityEl.value;
+  getWeather(searchTerm);
+  searchHistory.push(searchTerm);
+  localStorage.setItem("search", JSON.stringify(searchHistory));
+  renderSearchHistory();
+})
 
-      var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
+// Clear History button
+clearEl.addEventListener("click", function () {
+  localStorage.clear();
+  searchHistory = [];
+  renderSearchHistory();
+})
 
-      fetch(weatherURL)
-        .then(function (response) {
-          return response.json();
-        }).then(function (data) {
-          console.log(data);
-          console.log(data.main.humidity);
-          console.log(`humidity:${data.main.humidity}%`);
-          document.querySelector("#humidity").textContent = `humidity:${data.main.humidity}%`;
-          console.log(data.wind.speed);
-          console.log(`wind speed:${data.wind.speed}mph`);
-          document.querySelector("#wind-speed").textContent = `wind speed:${data.wind.speed}mph`;
-          console.log(data.main.temp);
-          console.log(`temp:${data.main.temp}F`);
-          document.querySelector("#temp").textContent = `temp:${data.main.temp}F`;
-          console.log(data.weather[0].icon);
-          var weatherIcon = data.weather[0].icon;
-          var iconURL = `http://openweathermap.org/img/w/${weatherIcon}.png`;
-          var weatherImage = document.createElement("img");
-          weatherImage.setAttribute("src", iconURL);
-          document.querySelector("#icon").appendChild(weatherImage);
-        });
-    }
-    );
+function k2f(K) {
+  return Math.floor((K - 273.15) * 1.8 + 32);
 }
 
+function renderSearchHistory() {
+  historyEl.innerHTML = "";
+  for (let i = 0; i < searchHistory.length; i++) {
+      const historyItem = document.createElement("input");
+      historyItem.setAttribute("type", "text");
+      historyItem.setAttribute("readonly", true);
+      historyItem.setAttribute("class", "form-control d-block bg-white");
+      historyItem.setAttribute("value", searchHistory[i]);
+      historyItem.addEventListener("click", function () {
+          getWeather(historyItem.value);
+      })
+      historyEl.append(historyItem);
+  }
+}
 
-document.querySelector("#search-button").addEventListener("click", function (event) {
-  event.preventDefault();
-  var cityname = document.querySelector("#city-input").value;
-  var requestUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityname},&limit=${5}&appid=${APIkey}`;
-  getApi(requestUrl);
-});
+renderSearchHistory();
+if (searchHistory.length > 0) {
+  getWeather(searchHistory[searchHistory.length - 1]);
+}
 
-//get data on the page
+}
+
+initPage();
 
 
-
-//loop through the data 5 day weather
-
-//display last searched city and you need to know the last city searched
-//need to save the last city searched in local storage
-//save the city name only in local storage and pass the city name
+    
