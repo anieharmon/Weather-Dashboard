455fc51443ebbc568a1fb41df77911c5e0a661ec
anieharmon <anieharmon@icloud.com>
2023-08-07 01:03:45 -0600

    icons

--- assets/js/script.js
+++ assets/js/script.js
@@ -85,6 +85,21 @@ function initPage() {
                        forecastDateEl.innerHTML = forecastMonth + "/" + forecastDay + "/" + forecastYear;
                        forecastEls[i].append(forecastDateEl);
 
+                       // Icon for current weather
+                       const forecastWeatherEl = document.createElement("img");
+                       forecastWeatherEl.setAttribute("src", "https://openweathermap.org/img/wn/" + response.data.list[forecastIndex].weather[0].icon + "@2x.png");
+                       forecastWeatherEl.setAttribute("alt", response.data.list[forecastIndex].weather[0].description);
+                       forecastEls[i].append(forecastWeatherEl);
+                       const forecastTempEl = document.createElement("p");
+                       forecastTempEl.innerHTML = "Temp: " + k2f(response.data.list[forecastIndex].main.temp) + " &#176F";
+                       forecastEls[i].append(forecastTempEl);
+                       const forecastHumidityEl = document.createElement("p");
+                       forecastHumidityEl.innerHTML = "Humidity: " + response.data.list[forecastIndex].main.humidity + "%";
+                       forecastEls[i].append(forecastHumidityEl);
+                   }
+               })
+       });
+}
 
 
       var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
