72db6be9514dbf81cb4c13346a1898fb644c8132
anieharmon <anieharmon@icloud.com>
2023-08-07 01:02:03 -0600

    current weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,22 @@ function initPage() {
     // Execute a current weather get request from open weather api
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&appid=" + APIKey;
     axios.get(queryURL)
+    .then(function (response) {
+
+      todayweatherEl.classList.remove("d-none");
+
+      // Parse response to display current weather
+      const currentDate = new Date(response.data.dt * 1000);
+      const day = currentDate.getDate();
+      const month = currentDate.getMonth() + 1;
+      const year = currentDate.getFullYear();
+      nameEl.innerHTML = response.data.name + " (" + month + "/" + day + "/" + year + ") ";
+      let weatherPic = response.data.weather[0].icon;
+      currentPicEl.setAttribute("src", "https://openweathermap.org/img/wn/" + weatherPic + "@2x.png");
+      currentPicEl.setAttribute("alt", response.data.weather[0].description);
+      currentTempEl.innerHTML = "Temperature: " + k2f(response.data.main.temp) + " &#176F";
+      currentHumidityEl.innerHTML = "Humidity: " + response.data.main.humidity + "%";
+      currentWindEl.innerHTML = "Wind Speed: " + response.data.wind.speed + " MPH";
 
 
       var weatherURL = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${APIkey}&units=imperial`;
